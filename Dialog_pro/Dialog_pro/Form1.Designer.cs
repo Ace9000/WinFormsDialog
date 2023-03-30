10a28120c4abf391d886fc304e63c27284f177e2
Anastas Acevski <123334501+Ace9000@users.noreply.github.com>
2023-03-30 23:47:11 +0200

    Add files via upload

---
+++ Dialog_pro/Dialog_pro/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Dialog_pro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnColor = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnFolderBrowser = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtDialog = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(32, 54);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(156, 45);
+            this.btnColor.TabIndex = 0;
+            this.btnColor.Text = "Change Back Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnFolderBrowser
+            // 
+            this.btnFolderBrowser.Location = new System.Drawing.Point(32, 221);
+            this.btnFolderBrowser.Name = "btnFolderBrowser";
+            this.btnFolderBrowser.Size = new System.Drawing.Size(156, 45);
+            this.btnFolderBrowser.TabIndex = 1;
+            this.btnFolderBrowser.Text = "Folder Browser";
+            this.btnFolderBrowser.UseVisualStyleBackColor = true;
+            this.btnFolderBrowser.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(194, 230);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(411, 28);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnFont
+            // 
+            this.btnFont.Location = new System.Drawing.Point(32, 136);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(156, 45);
+            this.btnFont.TabIndex = 3;
+            this.btnFont.Text = "Font Dialog";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(194, 144);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(411, 28);
+            this.textBox2.TabIndex = 4;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Txt File |*.txt";
+            // 
+            // txtDialog
+            // 
+            this.txtDialog.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDialog.Location = new System.Drawing.Point(194, 364);
+            this.txtDialog.Name = "txtDialog";
+            this.txtDialog.Size = new System.Drawing.Size(411, 28);
+            this.txtDialog.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(32, 355);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(156, 45);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save As";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(730, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 241);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(755, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 40);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Select Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "sliki|*.jpg";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(755, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 40);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Printing";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 439);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtDialog);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnFolderBrowser);
+            this.Controls.Add(this.btnColor);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnFolderBrowser;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox txtDialog;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Dialog_pro/Dialog_pro/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Dialog_pro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult xx1 = colorDialog1.ShowDialog();
+            if (xx1 == DialogResult.OK)
+            {
+                BackColor = this.colorDialog1.Color;
+                btnColor.ForeColor = this.colorDialog1.Color;
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = ("C:\\Users\\anast\\OneDrive\\Desktop\\C#\\2022").ToString();
+            DialogResult xx2 = folderBrowserDialog1.ShowDialog();
+            btnFolderBrowser.ForeColor = this.colorDialog1.Color;
+            string fn;
+            fn = folderBrowserDialog1.SelectedPath;
+            //if(fn == "")
+            if (xx2 == DialogResult.Cancel)
+            {
+                MessageBox.Show("Please select any folder");
+                return;
+            }
+            else
+            {
+                textBox1.Text = fn;
+            }
+        }
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            btnFolderBrowser.Font = fontDialog1.Font;
+            btnColor.Font = fontDialog1.Font;
+            textBox2.Text = fontDialog1.Font.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            string fn;
+            fn = saveFileDialog1.FileName;
+            txtDialog.Text = fn;
+            
+        }
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                //MessageBox.Show(openFileDialog1.Multiselect.ToString());
+
+                if (!openFileDialog1.Multiselect)
+                    pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                else
+                {
+                    
+                    foreach (String xx4 in openFileDialog1.FileNames)
+                    {
+                        //MessageBox.Show(xx4);
+                        pictureBox1.Image = Image.FromFile(xx4);
+                        Application.DoEvents(); // go zabavuva procesot na programata
+                        Thread.Sleep(1000);// ja otvora sekoja slika posle edna sekunda
+                    }
+
+                }
+
+            }
+                
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            printDialog1.ShowDialog();
+        }
+    }
+}
